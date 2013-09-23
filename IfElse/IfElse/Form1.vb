81f01309108acdec0826440a6bc53923a0536903
Jonathan Bowles <bowl0071@umn.edu>
2013-09-23 15:26:18 -0500

    Jonathan Bowles Workday 3 9/23/13

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,8 +4,19 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
+
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("Error: Please type a number")
+            Return
+        End If
+
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
+        If num1 < 0 Or num2 < 0 Then
+            MessageBox.Show("Error: Please type a positive number")
+            Return
+        End If
+
         If num1 > num2 Then
             largerNum = num1
             txtResult.Text = "The larger number is " & num1
@@ -15,6 +26,7 @@ Public Class Form1
         Else
             txtResult.Text = "They are equal"
         End If
+        
 
     End Sub
 End Class
